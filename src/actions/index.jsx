5c6e7cbb98980dab6805980ad9409bd659c7a3f1
Esmaeil MIRZAEE <esmaeilmirzaee@gmail.com>
2021-01-05 09:23:09 -0500

    Send just a request per user id using lodash -- Refactor to a new approach

--- src/actions/index.jsx
+++ src/actions/index.jsx
@@ -4,8 +4,18 @@ import _ from 'lodash';
 // In order to metigate network request
 export const fetchPostsAndUsers = () => async (dispatch, getState) => {
   await dispatch(fetchPosts()); // stop here because we require posts
-  const usersId = _.uniq(_.map(getState().posts, 'userId'));
-  usersId.forEach((id) => dispatch(fetchUser(id))); // don't stop here because there is no further action
+
+  // The following two lines could be combined into a _.chain method
+  // const usersId = _.uniq(_.map(getState().posts, 'userId'));
+  // usersId.forEach((id) => dispatch(fetchUser(id))); // don't stop here because there is no further action
+
+  // The above two lines could be as follow benefiting _.chain
+  // Note: without value() the following line of code won't be executed;
+  _.chain(getState().posts)
+    .map('userId')
+    .uniq()
+    .forEach((id) => dispatch(fetchUser(id)))
+    .value();
 };
 
 export const fetchPosts = () => async (dispatch) => {
